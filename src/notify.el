09131ec2e429b6bb38fbed87ef3f9916c7410fd3
wingdust <995315823@qq.com>
2020-07-31 18:45:53 +0800

    refresh notify

--- src/notify.el
+++ src/notify.el
@@ -7,20 +7,18 @@
       (while (re-search-forward "SCHEDULED" nil t)
         (setq otitle (org-entry-get (point) "SCHEDULED"))
         (setq obody (nth 4 (org-heading-components)))
-        (setq odate (nth 0 (split-string (string-trim-right otitle ">"))))
+        (setq odate (nth 0 (split-string (string-trim otitle "<" ">"))))
         ;;(setq odate1 (nth 1 (split-string (string-trim-right otitle ">"))))
-        (setq otime (nth 2 (split-string (string-trim-right otitle ">"))))
+        (setq otime (nth 2 (split-string (string-trim otitle "<" ">"))))
 
 
         ;; send the notification
         (if (equal (format-time-string "%4Y-%2m-%2d" (current-time)) odate)
 
           (run-at-time otime nil (lambda ()
-                                   (
                                     (setq alert-id (w32-notification-notify  :title otime
                                                                              :body obody ))
                                     (when alert-id (w32-notification-close alert-id)))
-                                    )
                                    ))
 
 
@@ -30,8 +28,11 @@
         ;;(org-up-heading-safe)
         ;;(org-get-heading)
         ))))
-(nth 2 (split-string (string-trim-right "<2020-07-30 周四 22:00>" ">")))
+(nth 2 (split-string (string-trim-right "<2020-07-30 周四 22:00>" "<>")))
 <2020-07-30 周四 22:00>
+(string-trim "<2020-07-30 周四 22:00>" "<" ">")
+(funcall (lambda () (- 2 1)))
+
 
 (current-time-string)
 (current-time)
