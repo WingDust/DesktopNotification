2c7fca679ddc3053c185a0131c5ec85473bb50a3
wingdust <995315823@qq.com>
2020-11-30 21:18:24 +0800

    version update

--- src/notify.el
+++ src/notify.el
@@ -1,58 +1,123 @@
-(defun getorg()
-  ""
+(defun get-org()
+  "对一个Org 文件中设有"
   (interactive)
   (let (foo)
-    ;;(goto-line 1)
+    (goto-line 1) ;; 到第一行去
     (save-excursion
-      (while (re-search-forward "SCHEDULED" nil t)
+      (while (re-search-forward "SCHEDULED" nil t) ;; 会从光标当前行开始正则匹配 SCHEDULED
+        ;; <2020-11-19 周四 13:04>
         (setq otitle (org-entry-get (point) "SCHEDULED"))
+        ;; 工作
         (setq obody (nth 4 (org-heading-components)))
+        ;; TODO keyword
+        (setq okeyword (nth 2 (org-heading-components)))
         (setq odate (nth 0 (split-string (string-trim otitle "<" ">"))))
-        ;;(setq odate1 (nth 1 (split-string (string-trim-right otitle ">"))))
         (setq otime (nth 2 (split-string (string-trim otitle "<" ">"))))
+
+        ;; 分解时间字符串来完成时间加减
+        (setq num1 (string-to-number (nth 0 (split-string otime ":"))))
+        (setq num2 (nth 1 (split-string otime  ":")))
         ;; send the notification
-        (if (equal (format-time-string "%4Y-%2m-%2d" (current-time)) odate)
+        (if (equal (format-time-string "%4Y-%2m-%2d" (current-time)) odate) ;; 只有当它的日期与当前计算机日期在同一天才会发出
+            (if (string-equal "00" num2)
+                (if (string-equal "00" (number-to-string num1))
+                    (setq otime2 "23:59")
+                  (progn
+                    (setq num1 (number-to-string (- num1 1)))
+                    (setq otime2 (concat num1 ":" "59"))
+                    )
+                  )
+                (setq otime2 (concat (number-to-string num1) ":" (number-to-string (- num2 1))))
+              )
+          (progn
+            (run-at-time otime2 nil (lambda ()
+                                      (f-write-text (concat okeyword "\n" obody) 'utf-8
+                                                    "E:\\spacemacs\\emacs26-3\\.emacs.d\\site-lisp\\notify\\Record.txt")
+                                      ))
 
-          (run-at-time otime nil (lambda ()
-                                    (setq alert-id (w32-notification-notify  :title otime
-                                                                             :body obody ))
-                                    (when alert-id (w32-notification-close alert-id)))
-                                   ))
-        ;;(org-get-heading t t)
-        ;;(org-get-entry)
-        ;;(org-up-heading-all)
-        ;;(org-up-heading-safe)
-        ;;(org-get-heading)
-        ))))
+            (run-at-time otime nil (lambda ()
+                                     ;;(setq alert-id (w32-notification-notify  :title otime :body obody ))
+                                     ;;(when alert-id (w32-notification-close alert-id)))
+                                     (w32-shell-execute "runas"  "E:\\spacemacs\\emacs26-3\\.emacs.d\\site-lisp\\notify\\notify.exe"
+                                                        )))
+            )
+          )))))
 
 
 
 
+(if 1
+    (insert-buffer)
+  (insert)
+    (message "1")
+  (if 1
+      )
+  (progn
+    (message "2")
+    (message "3")
+    (message "4")
+    )
+    )
 
 
-(nth 2 (split-string (string-trim-right "<2020-07-30 周四 22:00>" "<>")))
-<2020-07-30 周四 22:00>
-(string-trim "<2020-07-30 周四 22:00>" "<" ">")
-(funcall (lambda () (- 2 1)))
 
 
-(current-time-string)
-(current-time)
-(seconds-to-time 86400)
-(format-time-string "%4Y-%2m-%2d" (time-add (current-time) ))
-(equal (format-time-string "%4Y-%2m-%2d"  (current-time)) "2020-07-30")
+;;(format-seconds  (parse-time-string "17:15"))
+;;(encode-time (parse-time-string "17:15"))
+;;
+;;(encode-time )
+;;(format-time-string (parse-time-string "17:15"))
+;;(time-to-seconds "17:15")
+;;(parse-time-string "17:15")
+;;
+;;(decode-time)
+;;(date-to-time "17:15")
+;;
+;;
+;;
+;;(w32-shell-execute "runas"  "E:\\spacemacs\\emacs26-3\\.emacs.d\\site-lisp\\notify\\notify.exe"
+;;                   ;;(concat "TODOs (format "%S" "a sd")))
+;;                   (concat "TODO" " " (format "%S" "工作")))
+;;
+;;(start-process-shell-command)
+;;(start-process)
+;;
+;;
+;;(f-write-text "工作\n我人" 'utf-8
+;;              "E:\\spacemacs\\emacs26-3\\.emacs.d\\site-lisp\\notify\\Record.txt")
+;;(w32-shell-execute "open"
+;;         "E:\\spacemacs\\emacs26-3\\.emacs.d\\site-lisp\\notify\\notify.exe")
+;;
+;;
+;;
+;;
+;;(nth 2 (split-string (string-trim-right "<2020-07-30 周四 22:00>" "<>")))
+;;(string-trim "<2020-07-30 周四 22:00>" "<" ">")
+;;(funcall (lambda () (- 2 1)))
+;;
+;;
+;;(current-time-string)
+;;(current-time)
+;;(seconds-to-time 86400)
+;;(format-time-string "%4Y-%2m-%2d" (time-add (current-time) ))
+;;(equal (format-time-string "%4Y-%2m-%2d"  (current-time)) "2020-07-30")
+;;
+;;
+;;(run-at-time "18:12" nil #'test1)
+;;
+;;(current-time-string)
+;;
+;;(let (x) 
+;;  (setq x '(("mary" . 1) 
+;;            ("mary" . 2)))
+;;  ;;(dolist x
+;;  (message (number-to-string (cdr (assoc "mary" x))))
+;;  ;;  )
+;;  )
 
-(defun test1 () 
-  (message "1"))
 
-(run-at-time "18:12" nil #'test1)
-
-(current-time-string)
-
-(let (x) 
-  (setq x '(("mary" . 1) 
-            ("mary" . 2)))
-  ;;(dolist x
-  (message (number-to-string (cdr (assoc "mary" x))))
-  ;;  )
-  )
+;;(org-get-heading t t)
+;;(org-get-entry)
+;;(org-up-heading-all)
+;;(org-up-heading-safe)
+;;(org-get-heading)
